85093102bfddccb92b9796507801a647bd288cae
alphacodes19 <sambodhgupta55@gmail.com>
2026-05-13 17:15:15 +0530

    first

--- app.py
+++ app.py
@@ -1,22 +1,21 @@
 import streamlit as st
 
+from src.screens.home_screen import home_screen
+from src.screens.teacher_screen import teacher_screen
+from src.screens.student_screen import student_screen
+
 def main():
-    st.header("this is title")
-    name = st.text_input("enter your name")
-    col1, col2 = st.columns(2, gap = "small")
-    
-    with col1:
-        if st.button("Display my name", type = "primary", key = 'btn1', width = 'strech'):
-            print("hi", name)
-    with col2:
-        if st.button("Bye", type = "secondary", key = 'btn2', width = 'stretch'):
-            print("bye", name)
+    if 'login_type' not in st.session_state:
+        st.session_state['login_type'] = None
+        
+    match st.session_state['login_type']:
+        case 'teacher':
+            teacher_screen()
             
-    st.markdown("""
-                <style>
-                        button{
-                            background: orange ! important;
-                        }
-                        </style>
-                """, unsafe_allow_html = True)
+        case 'student':
+            student_screen()
+            
+        case None:
+            home_screen()
+            
 main()
---
+++ src/screens/home_screen.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+def home_screen():
+    st.header('Home Screen')
+    
+    col1, col2 = st.columns(2)
+    
+    with col1:
+        if st.button('Teacher Portal'):
+            st.session_state['login_type'] = 'teacher'
+         
+    with col2:
+        if st.button('Student Portal'):
+            st.session_state['login_type'] = 'student'
