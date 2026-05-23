bf486c50db91c63f6d35e8afeffe093a68d3a6e0
alphacodes19 <sambodhgupta55@gmail.com>
2026-05-23 23:40:51 +0530

    refactor: remove unused home_screen function and clean up header_home

--- src/components/header.py
+++ src/components/header.py
@@ -1,17 +1,14 @@
 import streamlit as st
 
-
-def home_screen():
-    st.header('Home Screen')
     
 def header_home():
     col1, col2 = st.columns(2)
-    
+    st.markdown
     with col1:
         if st.button('Teacher Portal'):
             st.session_state['login_type'] = 'teacher'
             st.rerun()
-            
+    st.markdown      
     with col2:
         if st.button('Student Portal'):
             st.session_state['login_type'] = 'student'
